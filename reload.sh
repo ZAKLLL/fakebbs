0a9872d1959e403067ea13d25035fc939d46fb5f
zakl_company_wsl <1015849735@qq.com>
2024-10-10 13:49:23 +0800

    服务器自定义更新

---
+++ reload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+while true; do
+
+    # 等待2分钟
+    sleep 120
+    
+    # 检查 git pull 是否有更新
+    if git pull | grep -q 'Already up to date.'; then
+        echo "No changes detected. Skipping restart."
+    else
+        echo "Changes detected. Proceeding with restart."
+        
+        # 找到并停止当前运行的进程
+        PID=$(pgrep -f "uvicorn main:app")
+        if [ ! -z "$PID" ]; then
+            echo "Stopping current process (PID: $PID)..."
+            kill $PID
+            sleep 2  # 给进程一些时间来完全停止
+        fi
+
+        # 启动新的应用程序实例
+        echo "Starting new instance of the application..."
+        nohup uvicorn main:app --host 0.0.0.0 --port 80 --reload  >>log.log 2>&1 &
+        echo "Reload complete!"
+    fi
+
+done
